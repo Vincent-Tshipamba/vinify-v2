074ce2a3c8ec0bcdb135811ff2d3e07c20b18afb
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-06 18:53:08 +0100

    Update application locale to French and add French validation messages

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'accepted' => 'Le champ :attribute doit être accepté.',
+    'email' => 'Le champ :attribute doit être une adresse email valide.',
+    'max' => [
+        'string' => 'Le champ :attribute ne doit pas dépasser :max caractÃ¨res.',
+        'file' => 'Le fichier :attribute ne doit pas dépasser :max kilo-octets.',
+    ],
+    'min' => [
+        'string' => 'Le champ :attribute doit contenir au moins :min caractÃ¨res.',
+    ],
+    'required' => 'Le champ :attribute est obligatoire.',
+    'string' => 'Le champ :attribute doit être une chaîne de caractÃ¨res.',
+    'unique' => 'La valeur du champ :attribute est déjà utilisée.',
+    'mimes' => 'Le champ :attribute doit être un fichier de type :values.',
+    'file' => 'Le champ :attribute doit être un fichier.',
+    'attributes' => [
+        'name' => 'nom',
+        'email' => 'email',
+        'phone' => 'numéro de téléphone',
+        'university_name' => 'université',
+        'subject' => 'sujet',
+        'document' => 'document',
+        'message' => 'message',
+        'password' => 'mot de passe',
+    ],
+];
