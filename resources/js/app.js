9e99bbdb219d955ad510e4cd40a5e7a66a203a38
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-10 14:11:09 +0100

    Importing jquery and preline in the main js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 
-import { renderAsync } from 'docx-preview'
+import { renderAsync } from 'docx-preview';
+
+import $ from 'jquery';
+window.$ = window.jQuery = $
 
 import 'sweetalert2';
+import 'preline';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
+
