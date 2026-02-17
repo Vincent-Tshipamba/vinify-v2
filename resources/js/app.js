33be7155e7c9c3fa917d73f54dbdbc96edea40e0
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-17 13:58:10 +0100

    Importing docx-preview for docx files' preview and initialization of flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 
 import { renderAsync } from 'docx-preview';
+window.renderDocx = renderAsync;
+import * as docx from 'docx-preview';
+window.docx = docx;
+
+
+import 'flowbite';
 
 import $ from 'jquery';
 window.$ = window.jQuery = $
@@ -7,5 +13,13 @@ window.$ = window.jQuery = $
 import 'sweetalert2';
 import 'preline';
 import Swal from 'sweetalert2';
+import { initFlowbite } from 'flowbite';
 window.Swal = Swal;
 
+document.addEventListener('livewire:navigated', () => {
+    initFlowbite();
+
+    if (window.HSStaticMethods) {
+        window.HSStaticMethods.autoInit();
+    }
+});
